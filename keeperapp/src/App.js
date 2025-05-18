15249abd0b06e2b388600775f36d99a866415a54
SURAJGUPTA0414 <suraj.gupta@capgemini.com>
2025-05-18 19:15:18 +0530

    my keeper commit

---
+++ keeperapp/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Note from "./Components/Note";
+import CreateArea from "./Components/CreateArea";
+
+function App() {
+  let [notes, setNotes] = useState([]);
+
+  function onClickHandler(inputText) {
+    setNotes([...notes, inputText]);
+  }
+
+  function deleteNote(id) {
+    setNotes((prevValue) => {
+      return prevValue.filter((note) => {
+        return note !== notes[id];
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onClick={onClickHandler} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onClick={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
